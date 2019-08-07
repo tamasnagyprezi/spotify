c7f736706b1d3fd2389e245ddcb89e0a54009a00
bkil <tamas.nagy@prezi.com>
2019-08-07 17:52:39 +0200

    WIP TamasTest: extract from Main so we don't conflict during pulls

---
+++ example-workshop/src/main/scala/io.scarman.spotify/TamasTest.scala
@@ -0,0 +1,45 @@
+package io.scarman.spotify
+
+import scala.concurrent.Future
+
+class TamasTest extends WithSpotify {
+  def wip(): Unit = {
+    val barthezz = Artist("2ikrIhCYSKmzCgWb9EM3aT")
+    await(barthezz.topTracks()()).tracks.map(_.name).foreach(println)
+
+    val oneWithYou = Track("3eA6NakkA6fFaMTw1sTYZ5")
+    println(await(oneWithYou.getAudioAnalysis()))
+    println(await(oneWithYou.getAudioFeatures()).valence)
+
+    val apacukaFriday = Playlist("3YfIjWjxZbjxS2NiOOkpve")
+    val list          = await(apacukaFriday())
+    println(list.name)
+    val sorted = list.tracks.items.map(t => (t.track.name, t.track.popularity))
+    sorted.sortBy(_._2).foreach(println)
+
+    val berci = Playlist("5cNp1xS22nNOwVtLMIm8og")
+    println(await(berci()))
+
+    val relatedToMonday = getRelatedArtists(Playlist("1M0w0CabcTplJJGyfbDPKG"))
+    println(relatedToMonday)
+
+    val friday = Playlist("3YfIjWjxZbjxS2NiOOkpve")
+    val from   = await(friday())
+    from.tracks.items.collect {
+      case t if relatedToMonday.intersect(t.track.artists.map(_.id).toSet).nonEmpty =>
+        println(t.track.name)
+    }
+  }
+
+  def getRelatedArtists(playlist: request.Playlist): Set[String] = {
+    val list       = await(playlist())
+    val artists    = list.tracks.items.flatMap(t => t.track.artists.map(_.id))
+    val getRelated = artists.map(Artist(_).relatedArtists()().map(_.artists.map(_.id)))
+    await(Future.sequence(getRelated)).flatten.toSet
+  }
+
+  def run(): Unit = {
+    val apacukaFriday = Playlist("3YfIjWjxZbjxS2NiOOkpve")
+    val list          = await(apacukaFriday())
+  }
+}
