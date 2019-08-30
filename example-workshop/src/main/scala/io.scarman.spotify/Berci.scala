e1bf5a13ffe1d5ba961fa948b8ec496023d60aef
Albert Peto <albert.peto@prezi.com>
2019-08-30 15:16:18 +0200

    Reorganize code for related artist based matching

--- example-workshop/src/main/scala/io.scarman.spotify/Berci.scala
+++ example-workshop/src/main/scala/io.scarman.spotify/Berci.scala
@@ -1,33 +1,54 @@
 package io.scarman.spotify
 
+import io.scarman.spotify.Berci.TrackWithRelatedArtists
+import io.scarman.spotify.request.Playlist
+
 import scala.concurrent.Future
 
 class Berci extends WithSpotify {
 
   def run(): Unit = {
 
-    val myPlaylist = Playlist("31GOsSXZrkUQgULwizcj2O")
-    val datawing = Playlist("5cNp1xS22nNOwVtLMIm8og")
-
-    // WARNING: Be careful with rewriting this as it can result in quickly exceeding the rate limits
-    val artistLimit = 1
+    val myPlaylistRequest = Playlist("31GOsSXZrkUQgULwizcj2O")
+    val datawingRequest = Playlist("3vljBAykhTtxl5ZZv8zuua")
 
     val artists = for {
-      myTracks <- myPlaylist()
-      datawingTracks <- datawing()
-      myArtists = myTracks.tracks.items.flatMap(_.track.artists).distinct
-      myRelatedArtists <- getRelatedArtists(myArtists.take(artistLimit))
-      moreRelatedArtists <- getRelatedArtists(myRelatedArtists.take(artistLimit))
-      datawingArtists = datawingTracks.tracks.items.flatMap(_.track.artists).distinct
-      matchingArtists = (myArtists ::: myRelatedArtists ::: moreRelatedArtists).distinct
-        .filter(a => datawingArtists.map(_.id).contains(a.id))
-    } yield matchingArtists
+      myTracks <- getTracksWithRelatedArtists(myPlaylistRequest)
+      datawingTracks <- getTracksWithRelatedArtists(datawingRequest)
+    } yield comparePlaylistTracks(myTracks, datawingTracks)
 
-    await(artists).foreach(a => println(a.name))
+    await(artists)
   }
+
+  def comparePlaylistTracks(l1: List[TrackWithRelatedArtists], l2: List[TrackWithRelatedArtists]) = {
+    for {
+      t1 <- l1
+      t2 <- l2
+    } {
+      val t1Artists = (t1.track.track.artists ::: t1.related).map(_.name).toSet
+      val t2Artists = (t2.track.track.artists ::: t2.related).map(_.name).toSet
+
+      val similarArtists = t1Artists.intersect(t2Artists)
+
+      if (similarArtists.nonEmpty) {
+        println(s"Similar: ${t1.track.track.name} and ${t2.track.track.name}: $similarArtists")
+      }
+    }
+  }
+
+  def getTracksWithRelatedArtists(playlistRequest: Playlist): Future[List[TrackWithRelatedArtists]] = for {
+    playlist <- playlistRequest()
+    tracks = playlist.tracks.items
+    relatedArtists <- Future.sequence(tracks.map(t => getRelatedArtists(t.track.artists)))
+  } yield tracks.zip(relatedArtists).map(p => TrackWithRelatedArtists(p._1,p._2))
 
   def getRelatedArtists(artists: List[io.scarman.spotify.response.Artist]): Future[List[io.scarman.spotify.response.Artist]] =
     Future.sequence(artists.map(a => Artist(a.id).relatedArtists()())).map { as =>
       as.flatMap(a => a.artists)
     }
+}
+
+object Berci {
+  case class TrackWithRelatedArtists(track: io.scarman.spotify.response.PlaylistTrack,
+                                     related: List[io.scarman.spotify.response.Artist])
 }
